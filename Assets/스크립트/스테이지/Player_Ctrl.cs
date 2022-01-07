77db0f3b56c47d6bf07ef82d3b8973f5fac319bf
kimsuin <su96in43@naver.com>
2022-01-07 21:04:13 +0900

    얍

---
+++ Assets/스크립트/스테이지/Player_Ctrl.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerState{
+	Run,
+	Jump,
+	D_Jump,
+	Death
+}
+
+public class Player_Ctrl : MonoBehaviour {
+	public PlayerState PS;
+	public float Jump_Power = 500f;
+	public AudioClip[] Sound;
+
+	public Animator animator;
+
+	public GameObject AnotherSpeaker;
+
+	void Update(){
+		rigidbody.WakeUp();
+		if(Input.GetKeyDown (KeyCode.Space) && PS != PlayerState.Death){
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				if(PS == PlayerState.Jump){
+					D_Jump ();
+				}
+				if(PS == PlayerState.Run){
+					Jump ();
+				}
+			}
+		}
+	}
+	
+	void Jump(){
+		PS = PlayerState.Jump;
+		rigidbody.AddForce (new Vector3 (0, Jump_Power, 0));
+		//SoundPlay (2);
+		AnotherSpeaker.SendMessage ("SoundPlay");
+
+		animator.SetTrigger("Jump");
+		animator.SetBool("Ground",false);
+	}
+
+	void D_Jump(){
+		PS = PlayerState.D_Jump;
+		rigidbody.AddForce (new Vector3 (0, Jump_Power, 0));
+		//SoundPlay (2);
+		AnotherSpeaker.SendMessage ("SoundPlay");
+
+		animator.SetTrigger("D_Jump");
+		animator.SetBool("Ground",false);
+	}
+	void Run(){
+		PS = PlayerState.Run;
+
+		animator.SetBool("Ground",true);
+	}
+	void OnCollisionEnter(Collision collision){
+		if(PS != PlayerState.Run && PS != PlayerState.Death){
+			Run();
+		}
+	}
+	void CoinGet(){
+		SoundPlay (0);
+	
+	}
+
+	void GameOver(){
+		PS = PlayerState.Death;
+		SoundPlay (1);
+	}
+
+	void SoundPlay(int Num){
+		audio.clip = Sound [Num];
+		audio.Play ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		rigidbody.WakeUp();
+		if (other.gameObject.name == "Coin") {
+			Destroy (other.gameObject);
+			CoinGet ();
+		}
+		if(other.gameObject.name == "DeathZone" && PS != PlayerState.Death){
+			GameOver();
+		}
+	}
+}
