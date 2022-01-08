68ca6982ace1e0af90d534780ac82aca3d301e98
kimsuin <su96in43@naver.com>
2022-01-08 14:56:15 +0900

    1월 7일 변경사항
    -점프 높이에 따른 장애물 크기조정
    -점프 소리 안나는 오류 수정
    -더블점프의 가변적 높이 수정
    -줌인/줌아웃기능
    -스테이지 장애물2,3,4 trigger처리
    -슬라이드 코드 작성(수정요함)

--- Assets/스크립트/스테이지/Player_Ctrl.cs
+++ Assets/스크립트/스테이지/Player_Ctrl.cs
@@ -5,54 +5,72 @@ public enum PlayerState{
 	Run,
 	Jump,
 	D_Jump,
+	Sliding,
 	Death
 }
 
 public class Player_Ctrl : MonoBehaviour {
+	
+	public CapsuleCollider collider;
+	
 	public PlayerState PS;
-	public float Jump_Power = 500f;
+	public float Jump_Power = 0.05f;
+	public float doubleJump_Power = 0.04f;
+	
+	public Rigidbody rig;
+	public float slideSpeed = 10f;
+	
 	public AudioClip[] Sound;
-
 	public Animator animator;
-
+	
 	public GameObject AnotherSpeaker;
-
+	
+	void Start()
+	{
+		collider = GetComponent<CapsuleCollider>();
+		rig = GetComponent<Rigidbody>();
+	}
+	
 	void Update(){
-		rigidbody.WakeUp();
-		if(Input.GetKeyDown (KeyCode.Space) && PS != PlayerState.Death){
-			if (Input.GetKeyDown (KeyCode.Space)) {
-				if(PS == PlayerState.Jump){
-					D_Jump ();
-				}
-				if(PS == PlayerState.Run){
-					Jump ();
-				}
+		rigidbody.WakeUp(); 
+		if(Input.GetKeyDown (KeyCode.Space) && PS != PlayerState.Death){ //점프키가 눌리고, 플레이어가 죽은 상태가 아니라면
+			if (Input.GetKeyDown (KeyCode.Space)) //점프키가 눌리면
+			if(PS == PlayerState.Jump){  //플레이어 상태가 점프라면 
+				D_Jump ();//더블점프 
+			}
+			if(PS == PlayerState.Run){ //플레이어 상태가 달리기라면
+				Jump (); //점프 
 			}
 		}
+		if (Input.GetKeyDown (KeyCode.LeftShift) && Input.GetKey (KeyCode.Z))
+			Sliding ();
+		else if (Input.GetKeyUp (KeyCode.LeftShift))
+			GoUp ();
 	}
 	
 	void Jump(){
 		PS = PlayerState.Jump;
-		rigidbody.AddForce (new Vector3 (0, Jump_Power, 0));
+		rigidbody.AddForce (Vector3.up *Jump_Power, ForceMode.Impulse);
 		//SoundPlay (2);
 		AnotherSpeaker.SendMessage ("SoundPlay");
-
+		
 		animator.SetTrigger("Jump");
 		animator.SetBool("Ground",false);
 	}
-
+	
 	void D_Jump(){
 		PS = PlayerState.D_Jump;
-		rigidbody.AddForce (new Vector3 (0, Jump_Power, 0));
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.AddForce (Vector3.up *doubleJump_Power, ForceMode.Impulse);
 		//SoundPlay (2);
 		AnotherSpeaker.SendMessage ("SoundPlay");
-
+		
 		animator.SetTrigger("D_Jump");
 		animator.SetBool("Ground",false);
 	}
 	void Run(){
 		PS = PlayerState.Run;
-
+		
 		animator.SetBool("Ground",true);
 	}
 	void OnCollisionEnter(Collision collision){
@@ -60,21 +78,32 @@ public class Player_Ctrl : MonoBehaviour {
 			Run();
 		}
 	}
+	
+	void Sliding()
+	{
+		transform.rotation = Quaternion.Euler(0,0,-90);
+	}
+	
+	void GoUp()
+	{
+		transform.rotation = Quaternion.Euler(0,0,0);
+	}
+	
 	void CoinGet(){
 		SoundPlay (0);
-	
+		
 	}
-
+	
 	void GameOver(){
 		PS = PlayerState.Death;
 		SoundPlay (1);
 	}
-
+	
 	void SoundPlay(int Num){
 		audio.clip = Sound [Num];
 		audio.Play ();
 	}
-
+	
 	void OnTriggerEnter(Collider other){
 		rigidbody.WakeUp();
 		if (other.gameObject.name == "Coin") {
---
+++ Assets/스크립트/스테이지3/Block_Loop1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block_Loop1 : MonoBehaviour {
+
+	public float Speed = 3;	//블럭들이 움직일 스피드
+	public GameObject[] Block;	//계속해서 만들 블럭
+	public GameObject A_Zone;	//가운데에 있는 블럭
+	public GameObject B_Zone;	//화면의 오른쪽에 있는 블럭
+
+	void Update(){
+		Move();
+	}
+	void Move(){
+		A_Zone.transform.Translate (Vector3.left * Speed * Time.deltaTime);
+		B_Zone.transform.Translate (Vector3.left * Speed * Time.deltaTime);
+
+		if (B_Zone.transform.position.x <= 0) {
+			Destroy (A_Zone);
+			A_Zone=B_Zone;
+			Make();
+			}
+	}
+	void Make(){
+		int A = Random.Range (0, Block.Length);
+		B_Zone = Instantiate(Block[A], new Vector3 (A_Zone.transform.position.x+30, -5, 0), transform.rotation) as GameObject;
+	}
+}
