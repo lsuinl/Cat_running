9ab1bdbc8ee7e370e7b8730ee483b8fc7e61ddb5
kimsuin <su96in43@naver.com>
2022-01-21 03:06:02 +0900

    1.20
    슬라이드 및 이동 코드 수정/무지개붕어빵(무적)설정/게임 오버화면 구현/일시정지화면 구현 짜쟌

--- Assets/스크립트/스테이지/Player_Ctrl.cs
+++ Assets/스크립트/스테이지/Player_Ctrl.cs
@@ -24,25 +24,28 @@ public class Player_Ctrl : MonoBehaviour {
 	public Animator animator;
 	
 	public GameObject AnotherSpeaker;
-	
+	public GameManager GM;
 	void Start()
 	{
 		collider = GetComponent<CapsuleCollider>();
 		rig = GetComponent<Rigidbody>();
 	}
-	
+	//------------------------------------------------------------
 	void Update(){
 		rigidbody.WakeUp(); 
 		if(Input.GetKeyDown (KeyCode.Space) && PS != PlayerState.Death){ //점프키가 눌리고, 플레이어가 죽은 상태가 아니라면
-			if (Input.GetKeyDown (KeyCode.Space)) //점프키가 눌리면
-				if(PS == PlayerState.Jump){  //플레이어 상태가 점프라면 
-					D_Jump ();//더블점프 
-				}
-				if(PS == PlayerState.Run){//플레이어 상태가 달리기라면
-					Jump (); //점프 
-				}
+			if(PS == PlayerState.Jump){  //플레이어 상태가 점프라면 
+				D_Jump ();//더블점프 
+			}
+			if(PS == PlayerState.Run){//플레이어 상태가 달리기라면
+				Jump (); //점프 
+			}
+			if(PS == PlayerState.Sliding){
+				GoUp();
+				Jump ();
+			}
 		}
-		if (Input.GetKeyDown (KeyCode.LeftShift))
+		if (Input.GetKey (KeyCode.LeftShift))//점프 도중에 슬라이드를 눌러도 캐릭터가 바닿에 내려가면 바로 슬라이드가 가능하도록 함(GetKey)
 			if (PS == PlayerState.Run)
 				Sliding ();
 		
@@ -50,7 +53,7 @@ public class Player_Ctrl : MonoBehaviour {
 			if (PS == PlayerState.Sliding)
 				GoUp ();
 	}
-	
+	//---------------------------------------------------------------
 	void Jump(){
 		PS = PlayerState.Jump;
 		rigidbody.AddForce (Vector3.up *Jump_Power, ForceMode.Impulse);
@@ -82,12 +85,11 @@ public class Player_Ctrl : MonoBehaviour {
 		}
 	}
 	
-
 	void Sliding()
 	{
 		PS = PlayerState.Sliding;
 		transform.rotation = Quaternion.Euler(0,0,-90);
-
+		rig.MovePosition(transform.position + Vector3.down); //캐릭터가 누울때 내려가는 움직임을 없애줌 
 		animator.SetTrigger("Sliding");
 		animator.SetBool("Ground",false);
 	}
@@ -95,20 +97,22 @@ public class Player_Ctrl : MonoBehaviour {
 	void GoUp()
 	{
 		transform.transform.rotation = Quaternion.Euler(0,0,0);
+		rig.MovePosition (transform.position + Vector3.up);
 		Run();
 
-		animator.SetTrigger("GoUp");
 		animator.SetBool("Ground",false);
 	}
+	//---------------------------------------------------------------------------------------------
 	
 	void CoinGet(){
 		SoundPlay (0);
-		
+		GM.CoinGet ();
 	}
 	
 	void GameOver(){
 		PS = PlayerState.Death;
 		SoundPlay (1);
+		GM.GameOver ();
 	}
 	
 	void SoundPlay(int Num){
@@ -122,8 +126,25 @@ public class Player_Ctrl : MonoBehaviour {
 			Destroy (other.gameObject);
 			CoinGet ();
 		}
-		if(other.gameObject.name == "DeathZone" && PS != PlayerState.Death){
+		else if (other.gameObject.tag == "Coin_R"){ //무지개 붕어빵이랑 닿으면
+			OnStrong(); //OnStrong 함수 실행
+			Destroy (other.gameObject);
+			CoinGet ();
+		}
+		
+		if(other.gameObject.name == "DeathZone" && PS != PlayerState.Death && this.gameObject.layer != 9){ //player layer가 9번이 아니라면
 			GameOver();
 		}
+	}
+	
+	void OnStrong()
+	{
+		this.gameObject.layer = 9; //player layer이 9번 (Strong)으로 바뀜
+		Invoke ("OffStrong", 5); //5초 후에는 OffStrong 함수 실행
+	}
+	
+	void OffStrong()
+	{
+		gameObject.layer = 0; //player layer이 Default로 바뀜
 	}
 }
---
+++ Assets/스크립트/스테이지/Send.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Send : MonoBehaviour {
+	public GameObject Target;
+	public string MethodName;
+
+	public void OnMouseDown(){
+				Target.SendMessage(MethodName);
+		}
+}
