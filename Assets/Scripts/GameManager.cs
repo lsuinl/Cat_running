060180b04544a8b30c3c5206dfbc4483701967fd
Hyosun Kim <ssunykkim@naver.com>
2022-01-31 16:11:51 +0900

    2022.01.31
    - intro scene 제작 + 모바일용 Input 코드 추가 (도희님)
    - Sliding, Double Jump 애니메이션화 및 적용 + 현의님이 그려주신 GUI 삽입

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState{
+	Play,
+	Pause,
+	End
+}
+
+public class GameManager : MonoBehaviour {
+
+	public GameState GS;
+
+	public GUIText Text_Meter;
+	public GUIText Text_Gold;
+	public GameObject Final_GUI;
+	public GUIText Final_Meter;
+	public GUIText Final_Gold;
+	public GameObject Pause_GUI;
+
+	public float Speed;
+	public float Meter;
+	public int Gold;
+
+	public void Update(){
+
+		if (GS == GameState.Play) {
+			Meter += Time.deltaTime * Speed;
+
+			Text_Meter.text = string.Format("{0:N0}m",Meter);
+		}
+	}
+
+	public void CoinGet(){
+
+		Gold += 1;
+		Text_Gold.text = string.Format ("{0}", Gold);
+	}
+
+	public void GameOver(){
+
+		Final_Meter.text = string.Format ("{0:N1}", Meter);
+		Final_Gold.text = string.Format ("{0}", Gold);
+
+		GS = GameState.End;
+		Final_GUI.SetActive (true);
+
+	
+	}
+
+	public void Replay(){
+		Time.timeScale = 1f;
+		Application.LoadLevel ("stage_1");
+
+	}
+
+	public void MainGo(){
+		Time.timeScale = 1f;
+		Application.LoadLevel ("stage_5");
+
+	}
+
+	public void Pause(){
+
+		GS = GameState.Pause;
+		Time.timeScale = 0f;
+		Pause_GUI.SetActive (true);
+	}
+
+	public void UnPause(){
+		
+		GS = GameState.Play;
+		Time.timeScale = 1f;
+		Pause_GUI.SetActive (false);
+	}
+
+}
